2e7661bf1d1e92c6cee11e8ed2581d986ad27b5d
Franco Agustín Abillera <franco.a.abillera@gmail.com>
2025-11-05 16:57:58 -0300

    Add Google Sheets form submission script

---
+++ Google_Sheet.js
@@ -0,0 +1,13 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbznlXrrkhgodQVUaYgtrAH9gixAlmJSP1WralcxX5TqsIfx9bQG1vZZWaoZzkZHY_Y/exec'
+
+const form = document.forms['contact-form']
+
+form.addEventListener('submit', e => {
+  
+  e.preventDefault()
+  
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => alert("¡Gracias! ¡¡Este formulario fue enviado!!" ))
+  .then(() => { window.location.reload(); })
+  .catch(error => error('Error!', error.message))
+})
