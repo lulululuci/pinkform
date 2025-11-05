13720b7895a67369627cb03d6b62d10f6642aa57
Franco Agustín Abillera <franco.a.abillera@gmail.com>
2025-11-05 18:36:27 -0300

    Update Google_Sheet.js

--- Google_Sheet.js
+++ Google_Sheet.js
@@ -6,8 +6,18 @@ form.addEventListener('submit', e => {
   
   e.preventDefault()
   
+  // Enviar a Google Sheets
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
   .then(response => alert("¡Gracias! ¡¡Este formulario fue enviado!!" ))
   .then(() => { window.location.reload(); })
-  .catch(error => console.error('Error: ' + error.message))
-})
+  .catch(error => console.error('Error!', error.message))
+
+    // Enviar con EmailJS
+  emailjs.sendForm('TU_SERVICE_ID', 'TU_TEMPLATE_ID', form)
+    .then(() => {
+      alert("¡Formulario enviado correctamente!");
+      window.location.reload();
+    }, (error) => {
+      alert("Error al enviar el correo: " + error);
+    });
+});
